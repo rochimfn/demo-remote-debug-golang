dda35bb95dd61bca2a66ea2ed91120e2be37128c
Rochim Farul Noviyan <rochim.noviyan@gmail.com>
2025-02-01 20:35:44 +0700

    Add push script to microk8s local registry

---
+++ scripts/push-image.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+microk8s ctr images rm localhost:32000/demo-remote-debug-golang:0.0.1
+docker tag demo-remote-debug-golang:0.0.1 localhost:32000/demo-remote-debug-golang:0.0.1
+docker push localhost:32000/demo-remote-debug-golang:0.0.1
+docker rmi localhost:32000/demo-remote-debug-golang:0.0.1
